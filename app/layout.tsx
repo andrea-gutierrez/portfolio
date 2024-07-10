f8c6bec224702bf0ab530258f23d6a5d5a44249f
andrea-gutierrez <andreguti93@gmail.com>
2024-07-10 21:54:21 +0800

    finalize the main page: UI and Colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,12 @@ import {Roboto_Condensed} from "next/font/google";
 
 import "./globals.css";
 
-import {Navbar} from "@/components";
-
 const inter = Roboto_Condensed({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Maribel Gutierrez",
     description: "Frontend and Fullstack developer",
+    keywords: "Front End, FullStack, Software Engineer"
 };
 
 export default function RootLayout({children}: Readonly<{
@@ -18,7 +17,6 @@ export default function RootLayout({children}: Readonly<{
     return (
         <html lang="en">
         <body className={inter.className}>
-        {/*<Navbar/>*/}
         {children}
         </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,30 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main
-            className="flex flex-row gap-5 justify-center my-auto bg-gradient-to-b from-blue-950 to-cyan-700 h-screen">
+            className="flex flex-row gap-5 justify-center my-auto h-screen main--bg">
             <div className="flex flex-col w-1/2 justify-center gap-5">
-                <div className="flex flex-col items-center justify-center border--main__top">
-                    <p className="text-5xl">Hi, I am <span
-                        className="text-yellow-700 font-bold">Maribel Gutierrez</span>!</p>
+                <div className="flex flex-col items-center justify-center">
+                    <h1>Hi there, I am <span
+                        className="highlighted">Maribel Gutierrez!</span></h1>
 
-                    <p className="text-2xl">I am a software engineer with B.Eng. in Electronic with over 6 years of
+                    <p className="main--text">I am a software engineer with B.Eng. in Electronic with over 6 years of
                         experience as a
-                        Front-End and Fullstack developer</p>
+                        Front-End and Fullstack developer. I have experience in industries such as software development,
+                        industrial automation and also in the IoT.</p>
                 </div>
-                <button className="rounded border-red-500 border-2 text-yellow-300 w-40 p-2 mx-auto">Resume</button>
+                <button className="rounded w-40 p-2 mx-auto button--primary">Resume</button>
 
-                {/*<div className="flex flex-row gap-3 justify-center text-lg mt-4">
-                    <Link href="/about" className="outline outline-offset-2 outline-cyan-500">About me</Link>
-                    <Link href="/project" className="outline outline-offset-2 outline-cyan-500">Projects</Link>
+                <ul className="flex flex-row gap-3 justify-center text-lg mt-4 mx-auto section--list">
+                    <Link href="/about">About me</Link>
+                    <Link href="/project">Projects</Link>
                     <Link href="/recommendation"
-                          className="outline outline-offset-2 outline-cyan-500">Recommendations</Link>
-                    <Link href="/contact" className="outline outline-offset-2 outline-cyan-500">Contact me</Link>
-                </div>*/}
+                    >Recommendations</Link>
+                    <Link href="/contact">Contact me</Link>
+                </ul>
             </div>
-            <div className="flex items-center justify-center">
-                <Image className="rounded-lg rotate-12" style={{height: '500px', width: 'auto'}} height={0} width={0} sizes="100vw" priority={true} alt="maribel_profile"
+            <div className="profile--container--image">
+                <Image className="profile--image" height={250} width={250}
+                       sizes="100vw" priority={true} alt="maribel_profile"
                        src="/images/profile/maribel_1.jpg"/>
             </div>
         </main>
